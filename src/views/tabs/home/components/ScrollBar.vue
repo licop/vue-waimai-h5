34b0c23a3aba274dec44eb19c617859ad3fc22ba
licop <licop@users.noreply.github.com>
2023-04-12 22:38:39 +0800

    feat: 商品列表页框架

--- src/views/tabs/home/components/ScrollBar.vue
+++ src/views/tabs/home/components/ScrollBar.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { useInterval } from '@/use/useInterval'
-import { useTimeout } from '@/use/useTimeout'
 import type { IScrollBarInfo } from '@/types'
 
 interface IProps {
